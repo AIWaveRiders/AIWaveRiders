4f62157f5a7763b49ee9335350e1f49de3044bea
Soren Larsen <iamsorenl@gmail.com>
2024-01-08 17:31:36 -0800

    update

--- Tutorial/pipe1.py
+++ Tutorial/pipe1.py
@@ -1,8 +1,55 @@
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import torch.nn.functional as F
 
 classifier = pipeline("sentiment-analysis")
 
 res = classifier("I've been waiting for a HuggingFace course my whole life.")
 
 print(res)
+
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+#res = classifier("I've been waiting for a HuggingFace course my whole life.")
+X_train = ["I've been waiting for a HuggingFace course my whole life.",
+           "Python is great!"]
+res = classifier(X_train)
+
+print(res)
+
+"""
+sequence = "Using a Transformer network is simple"
+res = tokenizer(sequence)
+print(res)
+tokens = tokenizer.tokenize(sequence)
+print(tokens)
+ids = tokenizer.convert_tokens_to_ids(tokens)
+print(ids)
+decoded_string = tokenizer.decode(ids)
+print(decoded_string)
+"""
+
+"""
+batch = tokenizer(X_train, padding=True, max_length=512, return_tensors="pt")
+print(batch)
+
+with torch.no_grad():
+    outputs = model(**batch)
+    print(outputs)
+    predictions = F.softmax(outputs.logits, dim=1)
+    print(predictions)
+    labels = torch.argmax(predictions, dim=1)
+    print(labels)
+"""
+
+save_directory = "saved"
+tokenizer.save_pretrained(save_directory)
+model.save_pretrained(save_directory)
+
+tok = AutoTokenizer.from_pretrained(save_directory)
+mod = AutoModelForSequenceClassification.from_pretrained(save_directory)
